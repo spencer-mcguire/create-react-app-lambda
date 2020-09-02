e2ea9ea0a095612a1dd51a64eada9341e371849d
spencer-mcguire <smcguire1826@gmail.com>
2020-09-02 13:29:36 -0500

    start auth

--- src/App.js
+++ src/App.js
@@ -1,50 +1,60 @@
-import React, { Component } from "react"
-import logo from "./logo.svg"
-import "./App.css"
+import React, { Component, useEffect } from 'react';
+
+import netlifyIdentity from 'netlify-identity-widget';
+
+import logo from './logo.svg';
+import './App.css';
 
 class LambdaDemo extends Component {
   constructor(props) {
-    super(props)
-    this.state = { loading: false, msg: null }
+    super(props);
+    this.state = { loading: false, msg: null };
   }
 
-  handleClick = api => e => {
-    e.preventDefault()
+  handleClick = (api) => (e) => {
+    e.preventDefault();
 
-    this.setState({ loading: true })
-    fetch("/.netlify/functions/" + api)
-      .then(response => response.json())
-      .then(json => this.setState({ loading: false, msg: json.msg }))
-  }
+    this.setState({ loading: true });
+    fetch('/.netlify/functions/' + api)
+      .then((response) => response.json())
+      .then((json) => this.setState({ loading: false, msg: json.msg }));
+  };
 
   render() {
-    const { loading, msg } = this.state
+    const { loading, msg } = this.state;
 
     return (
       <p>
-        <button onClick={this.handleClick("hello")}>{loading ? "Loading..." : "Call Lambda"}</button>
-        <button onClick={this.handleClick("async-dadjoke")}>{loading ? "Loading..." : "Call Async Lambda"}</button>
+        <button onClick={this.handleClick('hello')}>
+          {loading ? 'Loading...' : 'Call Lambda'}
+        </button>
+        <button onClick={this.handleClick('async-dadjoke')}>
+          {loading ? 'Loading...' : 'Call Async Lambda'}
+        </button>
         <br />
         <span>{msg}</span>
       </p>
-    )
+    );
   }
 }
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <LambdaDemo />
-        </header>
-      </div>
-    )
-  }
-}
+const App = () => {
+  useEffect(() => {
+    netlifyIdentity.init({});
+  }, []);
 
-export default App
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+        <button onClick={() => netlifyIdentity.open()}> LOG IN</button>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <LambdaDemo />
+      </header>
+    </div>
+  );
+};
+
+export default App;
