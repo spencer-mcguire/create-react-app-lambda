3fd5b89b12a542841a7d8489f5a74d0e8fc85748
spencer-mcguire <smcguire1826@gmail.com>
2020-09-02 16:27:50 -0500

    change: different syntax

--- src/lambda/identity-signup.js
+++ src/lambda/identity-signup.js
@@ -1,8 +1,8 @@
-exports.handler = async (event) => {
+export async function handler(event) {
   const { user } = JSON.parse(event.body);
   console.log(JSON.stringify(user, null, 2));
 
   return JSON.stringify({
     app_metadata: { roles: ['sub:free'] },
   });
-};
+}
