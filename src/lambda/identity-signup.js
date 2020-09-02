33b8f139382b902206b6e66132899e0fc00a27d9
spencer-mcguire <smcguire1826@gmail.com>
2020-09-02 16:44:34 -0500

    remove sign up

--- src/lambda/identity-signup.js
+++
@@ -1,8 +0,0 @@
-export async function handler(event) {
-  const { user } = JSON.parse(event.body);
-  console.log(JSON.stringify(user, null, 2));
-
-  return JSON.stringify({
-    app_metadata: { roles: ['sub:free'] },
-  });
-}
