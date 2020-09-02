175796184828e828dc8a97f375d50a4f47faadb7
spencer-mcguire <smcguire1826@gmail.com>
2020-09-02 15:33:45 -0500

    START: setup sign up function

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { Component, useState, useEffect } from 'react';
 import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
 import netlifyAuth from './netlifyAuth';
 import {
@@ -55,7 +55,7 @@ const App = () => {
         <Switch>
           <PublicRoute exact path='/' component={Welcome} />
           <PublicRoute exact path='/welcome' component={Welcome} />
-          <PrivateRoute path='/home' component={Hidden} />
+          <PrivateRoute path='/home' component={LambdaDemo} />
         </Switch>
       </BrowserRouter>
       <div className='App'>
@@ -80,35 +80,35 @@ const App = () => {
 
 export default App;
 
-// class LambdaDemo extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = { loading: false, msg: null };
-//   }
+class LambdaDemo extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { loading: false, msg: null };
+  }
 
-//   handleClick = (api) => (e) => {
-//     e.preventDefault();
+  handleClick = (api) => (e) => {
+    e.preventDefault();
 
-//     this.setState({ loading: true });
-//     fetch('/.netlify/functions/' + api)
-//       .then((response) => response.json())
-//       .then((json) => this.setState({ loading: false, msg: json.msg }));
-//   };
+    this.setState({ loading: true });
+    fetch('/.netlify/functions/' + api)
+      .then((response) => response.json())
+      .then((json) => this.setState({ loading: false, msg: json.msg }));
+  };
 
-//   render() {
-//     const { loading, msg } = this.state;
+  render() {
+    const { loading, msg } = this.state;
 
-//     return (
-//       <p>
-//         <button onClick={this.handleClick('hello')}>
-//           {loading ? 'Loading...' : 'Call Lambda'}
-//         </button>
-//         <button onClick={this.handleClick('async-dadjoke')}>
-//           {loading ? 'Loading...' : 'Call Async Lambda'}
-//         </button>
-//         <br />
-//         <span>{msg}</span>
-//       </p>
-//     );
-//   }
-// }
+    return (
+      <p>
+        <button onClick={this.handleClick('hello')}>
+          {loading ? 'Loading...' : 'Call Lambda'}
+        </button>
+        <button onClick={this.handleClick('async-dadjoke')}>
+          {loading ? 'Loading...' : 'Call Async Lambda'}
+        </button>
+        <br />
+        <span>{msg}</span>
+      </p>
+    );
+  }
+}
