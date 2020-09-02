9bcb510892797b32e7ddd084bcc13bf4373276c8
spencer-mcguire <smcguire1826@gmail.com>
2020-09-02 14:03:05 -0500

    test: auth

--- src/App.js
+++ src/App.js
@@ -1,60 +1,82 @@
-import React, { Component, useEffect } from 'react';
-
-import netlifyIdentity from 'netlify-identity-widget';
+import React, { useState, useEffect } from 'react';
+import netlifyAuth from './netlifyAuth';
 
 import logo from './logo.svg';
 import './App.css';
 
-class LambdaDemo extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { loading: false, msg: null };
-  }
+const App = () => {
+  let [loggedIn, setLoggedIn] = useState(netlifyAuth.isAuthenticated);
+  let [user, setUser] = useState(null);
 
-  handleClick = (api) => (e) => {
-    e.preventDefault();
+  useEffect(() => {
+    netlifyAuth.initialize((user) => {
+      setLoggedIn(!!user);
+    });
+  }, []);
 
-    this.setState({ loading: true });
-    fetch('/.netlify/functions/' + api)
-      .then((response) => response.json())
-      .then((json) => this.setState({ loading: false, msg: json.msg }));
+  let login = () => {
+    netlifyAuth.authenticate((user) => {
+      setLoggedIn(!!user);
+      setUser(user);
+      netlifyAuth.closeModal();
+    });
   };
 
-  render() {
-    const { loading, msg } = this.state;
-
-    return (
-      <p>
-        <button onClick={this.handleClick('hello')}>
-          {loading ? 'Loading...' : 'Call Lambda'}
-        </button>
-        <button onClick={this.handleClick('async-dadjoke')}>
-          {loading ? 'Loading...' : 'Call Async Lambda'}
-        </button>
-        <br />
-        <span>{msg}</span>
-      </p>
-    );
-  }
-}
-
-const App = () => {
-  useEffect(() => {
-    netlifyIdentity.init({});
-  }, []);
+  let logout = () => {
+    netlifyAuth.signout(() => {
+      setLoggedIn(false);
+      setUser(null);
+    });
+  };
 
   return (
     <div className='App'>
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
-        <button onClick={() => netlifyIdentity.open()}> LOG IN</button>
+        {loggedIn ? (
+          <div>You are logged in!</div>
+        ) : (
+          <button onClick={login}>Log in here.</button>
+        )}
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <LambdaDemo />
       </header>
     </div>
   );
 };
 
 export default App;
+
+// class LambdaDemo extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = { loading: false, msg: null };
+//   }
+
+//   handleClick = (api) => (e) => {
+//     e.preventDefault();
+
+//     this.setState({ loading: true });
+//     fetch('/.netlify/functions/' + api)
+//       .then((response) => response.json())
+//       .then((json) => this.setState({ loading: false, msg: json.msg }));
+//   };
+
+//   render() {
+//     const { loading, msg } = this.state;
+
+//     return (
+//       <p>
+//         <button onClick={this.handleClick('hello')}>
+//           {loading ? 'Loading...' : 'Call Lambda'}
+//         </button>
+//         <button onClick={this.handleClick('async-dadjoke')}>
+//           {loading ? 'Loading...' : 'Call Async Lambda'}
+//         </button>
+//         <br />
+//         <span>{msg}</span>
+//       </p>
+//     );
+//   }
+// }
