0e65e1da7c6f939c7cad45c29ebed02558a37402
spencer-mcguire <smcguire1826@gmail.com>
2020-09-02 20:29:02 -0500

    wip: add key + string erros + add call to db

--- src/lambda/identity-signup.js
+++ src/lambda/identity-signup.js
@@ -5,9 +5,43 @@
 // https://www.netlify.com/blog/2019/02/21/the-role-of-roles-and-how-to-set-them-in-netlify-identity/
 // https://www.netlify.com/docs/functions/#identity-and-functions
 
+import NodeFetch from 'node-fetch';
+
 exports.handler = async function (event, context) {
   const { user } = JSON.parse(event.body);
   console.log(JSON.stringify(user, null, 2));
+
+  //netlify user ID
+  const netlifyID = user.id;
+
+  // stripe customer ID
+  const stripeID = 2;
+
+  //call to Fauna DB
+  const response = await NodeFetch('https://graphql.fauna.com/graphql', {
+    method: 'Post',
+    headers: {
+      Authorization: `Bearer ${process.env.FAUNA_SERVER_KEY}`,
+    },
+    body: JSON.stringify({
+      query: `
+      mutation($netlifyID: ID! $stripeID: ID!){
+        createUser( data:{netlifyID: $netlifyID, stripeID: $stripeID }){
+          netlifyID
+          stripeID
+        }
+      }`,
+      variables: {
+        netlifyID,
+        stripeID,
+      },
+    }),
+  })
+    .then((res) => res.JSON())
+    .catch((err) => console.error(JSON.stringify(err, null, 2)));
+
+  console.log({ response });
+
   return {
     statusCode: 200,
 
